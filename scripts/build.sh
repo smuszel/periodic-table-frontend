a362aad1c3bed44c4319bd3c9f65fc9213fcf337
Stan Muszel <stanislaw.muszel@gmail.com>
2019-02-19 20:51:52 +0100

    add docs

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,8 +4,9 @@ f () {
     cd ./packages/$1
     npm run build
     cp -f ../../dockerfile ./dist
-    docker build ./dist -t "github-stats-table:$1"
+    docker build ./dist -t "smuszel/periodic-table-of-frontend:$1"
     rm ./dist/dockerfile
+    docker push "smuszel/periodic-table-of-frontend:$1"
 }
 
 for p in $packages; do f $p & done
